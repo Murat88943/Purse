c2dad05331746e6be50256af4ab38999d99c9642
Muart <shakurovmurat@gmail.com>
2025-10-27 15:28:00 +0500

    1 пуш начало бэкэнда spring

---
+++ backend/Purse/src/main/java/com/example/Purse/PurseApplication.java
@@ -0,0 +1,13 @@
+package com.example.Purse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurseApplication.class, args);
+	}
+
+}
---
+++ backend/Purse/src/test/java/com/example/Purse/PurseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Purse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
